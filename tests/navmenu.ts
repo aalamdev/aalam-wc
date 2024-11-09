5fda0b83f98632f8428d83579b364b6b9edb4e57
Premkumar S <itprem112@gmail.com>
2024-11-09 12:41:49 +0530

    navmenu : implemented with proxy and persist flags

--- tests/navmenu.ts
+++ tests/navmenu.ts
@@ -2,10 +2,11 @@ import {fixture, expect, html, oneEvent} from '@open-wc/testing';
 import {AalamNavMenu} from "../src/navmenu";
 import {setViewport} from "@web/test-runner-commands";
 
-describe('aalam-dropdown', () => {
+describe('aalam-navmenu', () => {
     const template = `
 <div>
     <style>
+        aalam-navmenu {font-size:16px;font-family:serif}
         [slot=menu-item] {background:yellow;padding:20px}
         [slot=toggle-item] {background:orange;padding:20px;color:blue}
     </style>
@@ -41,7 +42,7 @@ describe('aalam-dropdown', () => {
             return nav.querySelectorAll('[slot=menu-item]');
         }
         const c_slot = (nav) => {
-            return nav.querySelectorAll('[slot=__collapsed_item]');
+            return nav.querySelectorAll('[slot=collapsed-item]');
         }
 
     it('is defined', async () => {
@@ -58,7 +59,7 @@ describe('aalam-dropdown', () => {
             expect(el.slot).to.equal('menu-item');
         }
         const check_dd_el = (el) => {
-            expect(el.slot).to.equal('__collapsed_item');
+            expect(el.slot).to.equal('collapsed-item');
         }
         let nav = el.querySelector('aalam-navmenu');
         expect(nav.direction).to.equal('first');
@@ -68,24 +69,24 @@ describe('aalam-dropdown', () => {
         nav._sorted_menu.forEach( (el) => {
             check_menu_el(el);
         });
-        expect(nav.children[0]).to.equal(nav._sorted_menu[0]);
-        expect(nav.children[8]).to.equal(nav._sorted_menu[1]);
-        expect(nav.children[2]).to.equal(nav._sorted_menu[2]);
-        expect(nav.children[9]).to.equal(nav._sorted_menu[3]);
-        expect(nav.children[10]).to.equal(nav._sorted_menu[4]);
-        expect(nav.children[3]).to.equal(nav._sorted_menu[5]);
-        expect(nav.children[5]).to.equal(nav._sorted_menu[6]);
-        expect(nav.children[4]).to.equal(nav._sorted_menu[7]);
+        expect(nav._slotted_menu_order[0]).to.equal(nav._sorted_menu[0]);
+        expect(nav._slotted_menu_order[8]).to.equal(nav._sorted_menu[1]);
+        expect(nav._slotted_menu_order[2]).to.equal(nav._sorted_menu[2]);
+        expect(nav._slotted_menu_order[9]).to.equal(nav._sorted_menu[3]);
+        expect(nav._slotted_menu_order[10]).to.equal(nav._sorted_menu[4]);
+        expect(nav._slotted_menu_order[3]).to.equal(nav._sorted_menu[5]);
+        expect(nav._slotted_menu_order[5]).to.equal(nav._sorted_menu[6]);
+        expect(nav._slotted_menu_order[4]).to.equal(nav._sorted_menu[7]);
 
         /*sorted dropdown*/
         nav._sorted_dd.forEach( (el) => {
             check_dd_el(el);
         });
         expect(c_slot(nav).length).to.equal(4);
-        expect(nav.children[6]).to.equal(nav._sorted_dd[0]);
-        expect(nav.children[1]).to.equal(nav._sorted_dd[1]);
-        expect(nav.children[7]).to.equal(nav._sorted_dd[2]);
-        expect(nav.children[11]).to.equal(nav._sorted_dd[3]);
+        expect(nav._slotted_menu_order[6]).to.equal(nav._sorted_dd[0]);
+        expect(nav._slotted_menu_order[1]).to.equal(nav._sorted_dd[1]);
+        expect(nav._slotted_menu_order[7]).to.equal(nav._sorted_dd[2]);
+        expect(nav._slotted_menu_order[11]).to.equal(nav._sorted_dd[3]);
 
         nav.setAttribute('direction', 'last');
 
@@ -94,24 +95,24 @@ describe('aalam-dropdown', () => {
             check_menu_el(el);
         });
         expect(m_slot(nav).length).to.equal(8);
-        expect(nav.children[0]).to.equal(nav._sorted_menu[0]);
-        expect(nav.children[8]).to.equal(nav._sorted_menu[1]);
-        expect(nav.children[2]).to.equal(nav._sorted_menu[2]);
-        expect(nav.children[9]).to.equal(nav._sorted_menu[3]);
-        expect(nav.children[10]).to.equal(nav._sorted_menu[4]);
-        expect(nav.children[3]).to.equal(nav._sorted_menu[5]);
-        expect(nav.children[5]).to.equal(nav._sorted_menu[6]);
-        expect(nav.children[4]).to.equal(nav._sorted_menu[7]);
+        expect(nav._slotted_menu_order[0]).to.equal(nav._sorted_menu[0]);
+        expect(nav._slotted_menu_order[8]).to.equal(nav._sorted_menu[1]);
+        expect(nav._slotted_menu_order[2]).to.equal(nav._sorted_menu[2]);
+        expect(nav._slotted_menu_order[9]).to.equal(nav._sorted_menu[3]);
+        expect(nav._slotted_menu_order[10]).to.equal(nav._sorted_menu[4]);
+        expect(nav._slotted_menu_order[3]).to.equal(nav._sorted_menu[5]);
+        expect(nav._slotted_menu_order[5]).to.equal(nav._sorted_menu[6]);
+        expect(nav._slotted_menu_order[4]).to.equal(nav._sorted_menu[7]);
 
         /*sorted dropdown*/
         nav._sorted_dd.forEach( (el) => {
             check_dd_el(el);
         });
         expect(c_slot(nav).length).to.equal(4);
-        expect(nav.children[6]).to.equal(nav._sorted_dd[0]);
-        expect(nav.children[1]).to.equal(nav._sorted_dd[1]);
-        expect(nav.children[7]).to.equal(nav._sorted_dd[2]);
-        expect(nav.children[11]).to.equal(nav._sorted_dd[3]);
+        expect(nav._slotted_menu_order[6]).to.equal(nav._sorted_dd[0]);
+        expect(nav._slotted_menu_order[1]).to.equal(nav._sorted_dd[1]);
+        expect(nav._slotted_menu_order[7]).to.equal(nav._sorted_dd[2]);
+        expect(nav._slotted_menu_order[11]).to.equal(nav._sorted_dd[3]);
 
         const el2 = await fixture(html`
 <div>
@@ -155,24 +156,24 @@ describe('aalam-dropdown', () => {
             check_menu_el(el);
         });
         expect(m_slot(nav).length).to.equal(8);
-        expect(nav.children[8]).to.equal(nav._sorted_menu[0]);
-        expect(nav.children[0]).to.equal(nav._sorted_menu[1]);
-        expect(nav.children[9]).to.equal(nav._sorted_menu[2]);
-        expect(nav.children[2]).to.equal(nav._sorted_menu[3]);
-        expect(nav.children[10]).to.equal(nav._sorted_menu[4]);
-        expect(nav.children[3]).to.equal(nav._sorted_menu[5]);
-        expect(nav.children[5]).to.equal(nav._sorted_menu[6]);
-        expect(nav.children[4]).to.equal(nav._sorted_menu[7]);
+        expect(nav._slotted_menu_order[8]).to.equal(nav._sorted_menu[0]);
+        expect(nav._slotted_menu_order[0]).to.equal(nav._sorted_menu[1]);
+        expect(nav._slotted_menu_order[9]).to.equal(nav._sorted_menu[2]);
+        expect(nav._slotted_menu_order[2]).to.equal(nav._sorted_menu[3]);
+        expect(nav._slotted_menu_order[10]).to.equal(nav._sorted_menu[4]);
+        expect(nav._slotted_menu_order[3]).to.equal(nav._sorted_menu[5]);
+        expect(nav._slotted_menu_order[5]).to.equal(nav._sorted_menu[6]);
+        expect(nav._slotted_menu_order[4]).to.equal(nav._sorted_menu[7]);
 
         /*sorted dropdown*/
         nav._sorted_dd.forEach( (el) => {
             check_dd_el(el);
         });
         expect(c_slot(nav).length).to.equal(4);
-        expect(nav.children[6]).to.equal(nav._sorted_dd[0]);
-        expect(nav.children[7]).to.equal(nav._sorted_dd[1]);
-        expect(nav.children[1]).to.equal(nav._sorted_dd[2]);
-        expect(nav.children[11]).to.equal(nav._sorted_dd[3]);
+        expect(nav._slotted_menu_order[6]).to.equal(nav._sorted_dd[0]);
+        expect(nav._slotted_menu_order[7]).to.equal(nav._sorted_dd[1]);
+        expect(nav._slotted_menu_order[1]).to.equal(nav._sorted_dd[2]);
+        expect(nav._slotted_menu_order[11]).to.equal(nav._sorted_dd[3]);
 
         const el3 = await fixture(html`
 <div>
@@ -217,31 +218,31 @@ describe('aalam-dropdown', () => {
             check_menu_el(el);
         });
         expect(m_slot(nav).length).to.equal(8);
-        expect(nav.children[8]).to.equal(nav._sorted_menu[0]);
-        expect(nav.children[0]).to.equal(nav._sorted_menu[1]);
-        expect(nav.children[9]).to.equal(nav._sorted_menu[2]);
-        expect(nav.children[2]).to.equal(nav._sorted_menu[3]);
-        expect(nav.children[10]).to.equal(nav._sorted_menu[4]);
-        expect(nav.children[3]).to.equal(nav._sorted_menu[5]);
-        expect(nav.children[5]).to.equal(nav._sorted_menu[6]);
-        expect(nav.children[4]).to.equal(nav._sorted_menu[7]);
+        expect(nav._slotted_menu_order[8]).to.equal(nav._sorted_menu[0]);
+        expect(nav._slotted_menu_order[0]).to.equal(nav._sorted_menu[1]);
+        expect(nav._slotted_menu_order[9]).to.equal(nav._sorted_menu[2]);
+        expect(nav._slotted_menu_order[2]).to.equal(nav._sorted_menu[3]);
+        expect(nav._slotted_menu_order[10]).to.equal(nav._sorted_menu[4]);
+        expect(nav._slotted_menu_order[3]).to.equal(nav._sorted_menu[5]);
+        expect(nav._slotted_menu_order[5]).to.equal(nav._sorted_menu[6]);
+        expect(nav._slotted_menu_order[4]).to.equal(nav._sorted_menu[7]);
 
         /*sorted dropdown*/
         nav._sorted_dd.forEach( (el) => {
             check_dd_el(el);
         });
         expect(c_slot(nav).length).to.equal(4);
-        expect(nav.children[6]).to.equal(nav._sorted_dd[0]);
-        expect(nav.children[7]).to.equal(nav._sorted_dd[1]);
-        expect(nav.children[1]).to.equal(nav._sorted_dd[2]);
-        expect(nav.children[11]).to.equal(nav._sorted_dd[3]);
+        expect(nav._slotted_menu_order[6]).to.equal(nav._sorted_dd[0]);
+        expect(nav._slotted_menu_order[7]).to.equal(nav._sorted_dd[1]);
+        expect(nav._slotted_menu_order[1]).to.equal(nav._sorted_dd[2]);
+        expect(nav._slotted_menu_order[11]).to.equal(nav._sorted_dd[3]);
     });
 
     it('shrink and expand', async () => {
         await setViewport({width:1484, height:900});
         const el = await fixture(template);
         let nav = el.querySelector('aalam-navmenu');
-        const mn_len = nav.children.length - 1;
+        const mn_len = nav._slotted_menu_order.length;
 
         expect(m_slot(nav).length).to.equal(8);
         expect(c_slot(nav).length).to.equal(4);
@@ -258,7 +259,7 @@ describe('aalam-dropdown', () => {
         };
         const el_wid = () => {
             return Array.prototype.indexOf.call(
-                nav.children, nav._sorted_dd[dd_len() - 1]);
+                nav._slotted_menu_order, nav._sorted_dd[dd_len() - 1]);
         }
         const menu_el = () => {
             if(dd_len() < 1)
@@ -271,14 +272,14 @@ describe('aalam-dropdown', () => {
             let i;
             for(i = 0; len < window.innerWidth * 0.7; i++) {
                 let ix = Array.prototype.indexOf.call(
-                    nav.children,
+                    nav._slotted_menu_order,
                     nav._sorted_menu[nav._sorted_menu.length - 1 -i]);
                 len += nav._slot_order_list[ix];
             }
             return i - 1;
         }
         const y = 900;
-        expect(nav.children[el_wid()].slot).to.equal('__collapsed_item');
+        expect(nav._slotted_menu_order[el_wid()].slot).to.equal('collapsed-item');
 
         await setViewport({width:1000, height:y});
         nav.dispatchEvent(new Event('resize', {bubbles:true}));
@@ -338,8 +339,9 @@ describe('aalam-dropdown', () => {
         /*check toggler element*/
         let i;
         let len = 0;
-        for(i = 0; i < nav._slot_order_list.length; i++)
+        for(i = 0; i < nav._slot_order_list.length; i++) {
             len += nav._slot_order_list[i];
+        }
 
         /*add 16 for body element's margin 8*/
         let full_x = Math.ceil(len * 100/70) + 16;
@@ -376,6 +378,156 @@ describe('aalam-dropdown', () => {
         nav.dispatchEvent(new Event('resize', {bubbles:true}));
         expect(m_slot(nav).length).to.equal(mn_len - 1);
         expect(nav._dd_toggler.style.display).to.equal('block');
+    });
+    it('proxy and persist', async () => {
+        const el = await fixture(html`
+<div>
+    <style>
+        [slot=menu-item] {background:yellow;padding:20px}
+        [slot=toggle-item] {background:orange;padding:20px;color:blue}
+    </style>
+    <div id='outer'
+        style='display:inline-block;width:100%;height:100px;background:pink'>
+        <div id='upper'
+            style='width:150px;height:200px;
+                   display:inline-block;background:blue'>
+        </div>
+        <div id='container'
+            style='width:70%;height:80px;background:green;display:inline-block'>
+            <aalam-navmenu>
+                <div id='a1' slot='menu-item' data-priority='4'>06abcde05</div>
+                <div id='b2' slot='menu-item' data-priority='2'>03bcdef02</div>
+                <div id='c3' slot='menu-item' data-priority='5'>08cdefg07</div>
+                <div id='d4' data-persist slot='collapsed-item' data-priority='7'>10defgh10</div>
+                <div id='e5' slot='menu-item' data-priority='9'>12efghi12</div>
+                <div id='f6' slot='menu-item' data-priority='8'>11fghij11</div>
+                <div id='g7' slot='menu-item' data-priority='1'><input id="inp" type="text" value="01ghijk01" /></div>
+                <div id='h8' data-proxy="h8" slot='menu-item' data-priority='2'><input type="text" value="02hijkl03" /></div>
+                <div id='h8' data-proxy="h8" slot='collapsed-item' data-priority='2'>h8*02hijkl03</div>
+                <div id='i9' data-proxy="i9" slot='menu-item' data-priority='4'><input type="text" value="check" /></div>
+                <div id='i9' data-proxy="i9" slot='collapsed-item' data-priority='4'>i9*05ijklm06</div>
+                <div id='j10' slot='menu-item' data-priority='5'>07jklmn08</div>
+                <div id='k11' data-proxy="k11" slot='collapsed-item' data-priority='6'>k11*09klmno09</div>
+                <div id='k11' data-proxy="k11" slot='menu-item' data-priority='6'><input type="text" value="09klmno09" /></div>
+                <div id='l12' data-persist slot='collapsed-item' data-priority='3'>04lmnop04</div>
+                <div id='tgl' slot='toggle-item'>toggler</div>
+            </aalam-navmenu>
+        </div>
+        <div id='lower'
+            style='width:150px;height:100px;background:red;display:inline-block'>
+        </div>
+    </div>
+</div>`);
+        const nav = el.querySelector('aalam-navmenu');
+        const mn_len = nav._slotted_menu_order.length;
+        const dd_len = () => {return nav._sorted_dd.length};
+        const el_wid = () => {
+            return Array.prototype.indexOf.call(
+                nav._slotted_menu_order, nav._sorted_dd[dd_len() - 1]);
+        }
+        const sl_order = (ix) => {
+            if(ix < 0)
+                return 0;
+            return Math.round(nav._slot_order_list[ix])
+        };
+        const par = () => {
+            return Math.round(nav.parentElement.getBoundingClientRect().width)};
+        const x = () => {return Math.round((par() + sl_order(el_wid()))*100/70)};
+        const rx = () => {return Math.round((par() - sl_order(el_wid()))*100/70)};
 
+        const isProxy = (el) => {
+            return el.hasAttribute('data-proxy');
+        }
+        const check_proxy_clp = (el) => {
+            let px = el.getAttribute('data-proxy');
+            let clp = nav.querySelector(`[data-proxy=${px}][slot=collapsed-item]`);
+            expect(clp.style.display).to.equal('block');
+            expect(el.style.display).to.equal('none');
+        }
+        const check_proxy_mnu = (el) => {
+            let px = el.getAttribute('data-proxy');
+            let mnu = nav.querySelector(`[data-proxy=${px}][slot=menu-item]`);
+            expect(mnu.style.display).to.equal('block');
+            expect(el.style.display).to.equal('none');
+        }
+        const check_not_proxy = (el) => {
+            let px = el.getAttribute('data-proxy');
+            expect(px).to.equal(null);
+        }
+        const tgl_disp = () => {
+            expect(nav._dd_toggler.style.display).to.equal('block');
+        }
+        const check_persist = () => {
+            let c = nav.querySelectorAll('[data-persist][slot=collapsed-item]');
+            expect(c.length).to.equal(2);
+            tgl_disp();
+        }
+
+
+        check_persist();
+        let sz = 0;
+        let i = 0;
+        for(i = 0;i < nav._sorted_menu.length;i++) {
+            let ix = Array.prototype.indexOf.call(
+                nav._slotted_menu_order, nav._sorted_menu[i]);
+            sz += nav._slot_order_list[ix];
+        }
+        sz = Math.ceil((sz + nav._tgl_width) * 100/70) + 16;
+
+        await setViewport({width:sz, height:200});
+        nav.dispatchEvent(new Event('resize', {bubbles:true}));
+        expect(nav._sorted_menu.length).to.equal(i);
+        check_persist();
+
+        let len = 0;
+        for(i = 0; i < nav._slot_order_list.length; i++)
+            len += nav._slot_order_list[i];
+
+        /*add 16 for body element's margin 8*/
+        let full_x = Math.ceil((len + nav._tgl_width) * 100/70) + 16;
+
+        check_persist();
+        await setViewport({width:full_x, height:200});
+        nav.dispatchEvent(new Event('resize', {bubbles:true}));
+        expect(nav._sorted_menu.length).to.equal(mn_len);
+        expect(nav._sorted_dd.length).to.equal(0);
+        check_persist();
+
+        await setViewport({width:rx(), height:200});
+        nav.dispatchEvent(new Event('resize', {bubbles:true}));
+        expect(nav._sorted_menu.length).to.equal(mn_len - 1);
+        expect(nav._sorted_dd.length).to.equal(1);
+        check_not_proxy(nav._sorted_dd[dd_len() - 1]);
+        let inp_el = nav.querySelector("#inp");;
+        expect(nav._sorted_dd[dd_len() - 1].children[0]).to.equal(inp_el)
+        check_persist();
+
+        await setViewport({width:rx(), height:200});
+        nav.dispatchEvent(new Event('resize', {bubbles:true}));
+        expect(nav._sorted_menu.length).to.equal(mn_len - 2);
+        expect(nav._sorted_dd.length).to.equal(2);
+        check_proxy_clp(nav._sorted_dd[dd_len() - 1]);
+        check_persist();
+
+        await setViewport({width:rx(), height:200});
+        nav.dispatchEvent(new Event('resize', {bubbles:true}));
+        expect(nav._sorted_menu.length).to.equal(mn_len - 3);
+        expect(nav._sorted_dd.length).to.equal(3);
+        check_not_proxy(nav._sorted_dd[dd_len() - 1]);
+        check_persist();
+
+        await setViewport({width:x(), height:200});
+        nav.dispatchEvent(new Event('resize', {bubbles:true}));
+        expect(nav._sorted_menu.length).to.equal(mn_len - 2);
+        expect(nav._sorted_dd.length).to.equal(2);
+        expect(isProxy(nav._sorted_dd[dd_len() - 1])).to.equal(true);
+        check_proxy_clp(nav._sorted_dd[dd_len() - 1])
+        check_persist();
+
+        await setViewport({width:full_x + full_x, height:200});
+        nav.dispatchEvent(new Event('resize', {bubbles:true}));
+        expect(nav._sorted_menu.length).to.equal(mn_len);
+        expect(nav._sorted_dd.length).to.equal(0);
+        check_persist();
     });
 });
